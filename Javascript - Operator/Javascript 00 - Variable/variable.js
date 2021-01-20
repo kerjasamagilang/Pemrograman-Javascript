f9e8ad7cfe8ffd1d4015951a10e4ca6c61ae36fc
kerjasamagilang <kerjasamagilang@gmail.com>
2021-01-20 15:47:59 +0700

    Variable dasar Javascript

---
+++ Javascript - Operator/Javascript 00 - Variable/variable.js
@@ -0,0 +1,15 @@
+// membuat variabel
+// var namaVariable = nilaiVariable;
+var name = "Gilang Purnama";
+var visitorCount = 50322;
+var isActive = true;
+var url = "https://www.instagram.com/inisialnyag";
+
+// menampilkan variabel ke jendela dialog (alert)
+alert("Selamat datang di " + name);
+
+// menampilkan variabel ke dalam HTML
+document.write("Nama Situs: " + name + "<br>");
+document.write("Jumlah Pengunjung: " + visitorCount + "<br>");
+document.write("Status Aktif: " + isActive + "<br>");
+document.write("Alamat URL: " + url + "<br>");
