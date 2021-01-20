e1a37fe055996f50db86eaea9592786d6ed431ce
kerjasamagilang <kerjasamagilang@gmail.com>
2021-01-20 16:07:54 +0700

    Operator Ternary
    Opertor ternary pada Javascript, biasanya digunakan untuk membuat sebuah percabangan if/else.

---
+++ Javascript - Operator/Javascript 06 - Operator Ternary/ternary.js
@@ -0,0 +1,12 @@
+// OPERASI TERNARY
+// <kodisi> ? "benar" : "salah"
+var pertanyaan = confirm("Apakah kamu berumur diatas 18 tahun?")
+
+var hasil = pertanyaan ? "Selamat datang" : "Kamu tidak boleh di sini";
+document.write(hasil);
+
+/*
+Perhatikan! <kondisi> dapat kita isi dengan ekspresi yang menghasilkan nilai true dan false.
+Apabila kondisi bernilai true, maka "benar" yang akan dipilih dan sebaliknya—apabila false—maka "salah" yang akan dipilih.
+Opertor ini unik, seperti membuat pertanyaan.
+*/
