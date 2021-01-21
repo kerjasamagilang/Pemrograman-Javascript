e104799342ffcc8c09bc6ba8ab78a60cf8841501
kerjasamagilang <kerjasamagilang@gmail.com>
2021-01-21 11:40:44 +0700

    Percabangan IF ELSE
    Percabangan IF ELSE merupakan percabangan yang memiliki dua blok kondisi. Pilihan pertama untuk kondisi benar, dan pilihan kedua untuk kondisi salah (else)

---
+++ Javascript - Control Flow Percabangan/Javascript 01 - IF ELSE/ifelse.js
@@ -0,0 +1,9 @@
+var password = prompt("Masukan Password anda");
+
+// Percabangan IF ELSE
+if (password == "gilang"){
+    document.write("Password anda benar, silakan masuk");
+} else {
+    document.write("Password salah, silahkan coba lagi!");
+}
+document.write("Terima kasih");
