655cb0ca8af419742ccadc7ce846155b6033d5d4
kerjasamagilang <kerjasamagilang@gmail.com>
2021-01-21 11:37:51 +0700

    Percabangan IF
    Percabangan IF merupakan percabangan yang hanya memiliki satu blok pilihan saat kondisi bernilai benar

---
+++ Javascript - Control Flow Percabangan/Javascript 00 - IF/if.js
@@ -0,0 +1,8 @@
+var totalBelanja = prompt("Total belanja?", 0);
+
+// Percabangan IF
+if (totalBelanja > 10000) {
+    document.write("Anda belanja lebih dari 10000, silakan ambil hadiah <br>");
+}
+document.write("Terima kasih telah berbelanja di sini");
+
