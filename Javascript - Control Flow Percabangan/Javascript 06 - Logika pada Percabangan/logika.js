f6f038c5903bfc45886cc7bd630e7c755fe5e4e4
kerjasamagilang <kerjasamagilang@gmail.com>
2021-01-21 11:54:04 +0700

    Pengkondisian dengan Logika
    Bentuk sederhana dari percabangan bersarang menggunakan operator logika

---
+++ Javascript - Control Flow Percabangan/Javascript 06 - Logika pada Percabangan/logika.js
@@ -0,0 +1,8 @@
+var username = prompt("Masukan USERNAME");
+var password = prompt("Masukan PASSWORD");
+
+if (username == "@gilang" && password == "gilang"){
+        document.write("Selamat datang Gilang")
+    } else {
+        document.write("Password atau username salah")
+    }
