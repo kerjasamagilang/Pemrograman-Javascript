f71633d593899388114a871e415e2294519a1ae0
kerjasamagilang <kerjasamagilang@gmail.com>
2021-01-25 13:53:41 +0700

    Perulangan Repeat
    Fungsi ini khusus digunakan untuk mengulang sebuah teks (string).

---
+++ Javascript - Pengulangan/Javascript 04 - Repeat/repeat.js
@@ -0,0 +1,7 @@
+// Perulangan dengen method atau fungsi repeat() termasuk dalam perulangan counted loop.
+// Fungsi ini khusus digunakan untuk mengulang sebuah teks (string).
+// Bisa dibilang
+// Ini merupakan singkat dari perulangan for.
+
+
+document.write("Ulangi kalimat ini! <br>".repeat(10));
