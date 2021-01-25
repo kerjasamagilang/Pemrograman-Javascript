861909b3bc63714d51a22f3ce0fb873287f85470
kerjasamagilang <kerjasamagilang@gmail.com>
2021-01-25 13:49:27 +0700

    Perulangan FOR
    Perulangan for merupakan perulangan yang termasuk dalam couted loop, karena sudah jelas berapa kali ia akan mengulang.

---
+++ Javascript - Pengulangan/Javascript 00 - For/for.js
@@ -0,0 +1,18 @@
+// Menghitung urutan nomor dari yang kecil hingga besar
+// Increment
+// hitung++
+for (hitung = 1; hitung <= 10; hitung++){
+    document.write("Urutan ke- " + hitung + "<br>");
+}
+
+// Menghitung urutan nomor dari yang besar hingga kecil
+// Decrement
+// hitungMundur--
+for (hitungMundur = 10; hitungMundur >= 1; hitungMundur--){
+    document.write("Perulangan ke- " + hitungMundur + "<br>")
+}
+
+// Menghitung urutan kelipatan 2 atau berapapun
+for (tambahDua = 0; tambahDua <= 20; tambahDua+=2){
+    document.write("Urutan ke- " + tambahDua + "<br>")
+}
